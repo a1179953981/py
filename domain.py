429ec41203ee74c40196f5b0f82a06a8f2819908
11 <1179953981@qq.com>
2020-06-10 11:45:06 +0800

    最新2020年6月10日

--- domain.py
+++ domain.py
@@ -80,7 +80,7 @@ def before_ding(hostname):
 
 
 # --------------钉钉自动告警--------------------
-def ding(hostname, times, days):
+def ding(hostname, times, days, where):
     webhook = "https://oapi.dingtalk.com/robot/send?access_token=0b80643a9fb64cfbe28edea2b0a528ead06ec37540e5efc458353dd163c5f35c"
     header = {
         "Content-Type": "application/json",
@@ -93,7 +93,9 @@ def ding(hostname, times, days):
             "text": "#### 域名到期预警\n" +
                     "> 警报域名:    " + str(hostname) + "\n\n"
                                                     "> 到期时间:   " + str(times) + "\n\n"
-                                                                                "> 剩余天数:   " + str(days) + "\n\n"
+                                                                                "> 剩余天数:   " + str(
+                days) + "\n\n""> 来源:   " + where + "\n\n"
+
         },
         "at": {
             "isAtAll": False,
@@ -168,7 +170,9 @@ def rs(root):
 
             for item in keywords:
                 # PATTERN = re.compile("([a-z]|[0-9])*.(com|net|cn|tv)")
-                tmp = re.finditer("(\w+)\.(co|com|net|cn|tv|me|top|fun|xyz|xin|shop|mobi|vip|info|ink|wang|sife|club|cc|online|biz|red|link|ltd|org|com.cn|net.cn|org.cn|gov.cn|name|pro|work|kim|group|tech|store|ren|pub|live|wiki|design|beer|art|luxe)$", item)
+                tmp = re.finditer(
+                    "(\w+)\.(co|com|net|cn|tv|me|top|fun|xyz|xin|shop|mobi|vip|info|ink|wang|sife|club|cc|online|biz|red|link|ltd|org|com.cn|net.cn|org.cn|gov.cn|name|pro|work|kim|group|tech|store|ren|pub|live|wiki|design|beer|art|luxe)$",
+                    item)
                 # print(item)
                 # print(tmp)
                 for result in tmp:
@@ -214,69 +218,95 @@ def apidata():
     return s
 
 
+# 读取需要过滤的域名文件
+exclude = rs(r"E:\out.txt")
+for t in exclude:
+    print("排除的域名", exclude)
 domainSet = rs(r"C:\Users\yonrun1001\Downloads\conf")
-domainSet = set.union(domainSet, apidata())
+domainSet = set.difference(domainSet, exclude)
 domainSetlen = len(domainSet)
-print("去重处理共%d条" % domainSetlen)
-num = 0
-for domain in domainSet:
-    # print("待处理域名==========================")
-    # print(domain)
-    num = num + 1
-    print("处理第%d条域名,共%d条:%s" % (num, domainSetlen, domain))
-    a_result = due(domain)
-    if a_result:
+print("服务器中去重处理共%d条,并排除域名后" % domainSetlen)
 
-        if a_result['success'] != '0':
-            if a_result['result']['status'] == 'WAIT_PROCESS':
-                print("%s:等待系统处理（预计两分钟后自动重试）" + domain)
-                ding(domain, "错误", "获取失败,等待系统处理")
-                continue
-                # due(hostname)
-                # time.sleep(130)
-                # 处理四次，还不成功则直接返回
-            # if a_result['msgid'] == '1000701':
-            #     print("超额，重试中,请无需关闭........"+domain)
-            #     due(domain)
 
-            if a_result['result']['status'] == 'ALREADY_WHOIS':
-                t2 = a_result['result']['dom_expdate']
-                try:
-                    t3 = int(time_passed(t2))
-                except:
-                    t3 = t2
-                if t3 < 30:
-                    ding(domain, t2, t3)
-                    print("不正常的域名:%s,已警报:到期时间为:%s,剩余天数:%s" % (domain, t2, t3))
-                    continue
+def end(domains, where, num):
+    for domain in domains:
+        # print("待处理域名==========================")
+        # print(domain)
+        num = num + 1
+        print("处理第%d条域名,共%d条:%s" % (num, domainSetlen, domain))
+        a_result = due(domain)
+
+        def continue1(result):
+            if a_result:
+
+                if a_result['success'] != '0':
+                    if a_result['result']['status'] == 'WAIT_PROCESS':
+                        print("%s:等待系统处理（预计两分钟后自动重试）" + domain)
+                        ding(domain, "获取失败", "获取失败,请求失败", where)
+                        # time.sleep(3 * 60)
+                        # continue1(due(domain))
+                        # ding(domain, "错误", "获取失败,等待系统处理", where)
+                        return
+                        # due(hostname)
+                        # time.sleep(130)
+                        # 处理四次，还不成功则直接返回
+                    # if a_result['msgid'] == '1000701':
+                    #     print("超额，重试中,请无需关闭........"+domain)
+                    #     due(domain)
+
+                    if a_result['result']['status'] == 'ALREADY_WHOIS':
+                        t2 = a_result['result']['dom_expdate']
+                        try:
+                            t3 = int(time_passed(t2))
+                            if t3 < 30:
+                                ding(domain, t2, t3, where)
+                                print("不正常的域名:%s,已警报:到期时间为:%s,剩余天数:%s,来源:%s" % (domain, t2, t3, where))
+                                return
+                        except:
+                            t3 = t2
+                            ding(domain, t2, t3, where)
+                            print("不正常的域名:%s,已警报:到期时间为:%s,剩余天数:%s,来源:%s" % (domain, t2, t3, where))
+                            return
+
+                        else:
+                            print("正常的域名:%s,到期时间为:%s,剩余天数:%d,来源:%s" % (domain, t2, t3, where))
+                            return
+
+                        if a_result['result']['status'] == 'NOT_REGISTER':
+                            print("未注册域名%s,来源:%s" % (domain, where))
+                            ding(domain, "获取失败", "获取失败,未注册域名", where)
+                            return
+                        if a_result['result']['status'] == 'BE_RETAINED':
+                            print("域名被保留%s,来源:%s" % (domain, where))
+                            ding(domain, "获取错误", "获取错误,域名被保留", where)
+                            return
+
+
                 else:
-                    print("正常的域名:%s,到期时间为:%s,剩余天数:%d" % (domain, t2, t3))
-                    continue
+                    # 如果超额，70分钟后自动继续
+                    if a_result['msgid'] == '1000701':
+                        print("超额，程序将在70分钟后继续执行,请无需关闭........" + domain)
+                        time.sleep(70 * 60)
+                        continue1(due(domain))
+                    if a_result['msgid'] == '10018':
+                        print("获取失败,未识别的域名%s,来源:%s" % (domain, where))
+                        ding(domain, "获取错误", "获取错误,未识别的域名", where)
+                        return
+                    print(a_result['msgid'] + ' ' + a_result['msg'] + domain)
+                    print("查询失败，正在重试。。。。。。" + domain)
+                    continue1(due(domain))
+            else:
+                print('Request nowapi fail.' + domain)
+                print("查询失败，正在重试。。。。。。" + domain)
+                time.sleep(5)
+                continue1(due(domain))
 
-                if a_result['result']['status'] == 'NOT_REGISTER':
-                    print("未注册域名" + domain)
-                    ding(domain, "获取失败", "获取失败,未注册域名")
-                    continue
-                if a_result['result']['status'] == 'BE_RETAINED':
-                    print("域名被保留" + domain)
-                    ding(domain, "获取错误", "获取错误,域名被保留")
-                    continue
+        continue1(a_result)
 
 
-        else:
-            # 如果超额，70分钟后自动继续
-            if a_result['msgid'] == '1000701':
-                print("超额，程序将在70分钟后继续执行,请无需关闭........" + domain)
-                time.sleep(70 * 60)
-                due(domain)
-            if a_result['msgid'] == '10018':
-                print("获取失败,未识别的域名" + domain)
-                ding(domain, "获取错误", "获取失败,未识别的域名")
-                continue
-            print(a_result['msgid'] + ' ' + a_result['msg'] + domain)
-            print("查询失败，正在重试。。。。。。" + domain)
-            due(domain)
-    else:
-        print('Request nowapi fail.' + domain)
-        print("查询失败，正在重试。。。。。。" + domain)
-        due(domain)
+end(domainSet, "主机文件", 0)
+domainSet2 = apidata()
+domainSet2 = set.difference(domainSet2, exclude)
+domainSetlen2 = len(domainSet2)
+print("baiyunshan中去重处理并去去除排除域名共%d条" % domainSetlen2)
+end(domainSet2, "baishanAPI", 0)
